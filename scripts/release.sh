ebf6ca35deb2a3728c11093eacedf656d82794d8
Parul Singhal <parul.singhal01@gmail.com>
2026-06-10 02:34:00 -0700

    release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,7 +7,7 @@ VERSION=$(date +%Y.%m.%d.%H%M)
 echo "New Version: v$VERSION"
 
 git tag -a "v$VERSION" -m "Release v$VERSION"
-git push origin 'v$VERSION"
+git push origin 'v$VERSION'
 
 echo "Release tagged successfully"
 
