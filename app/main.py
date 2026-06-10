7f1b414a19b091001fd914c4d1a149caaeb52c1b
Parul Singhal <parul.singhal01@gmail.com>
2026-06-10 02:28:14 -0700

    main.py

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/health")
 def health():
     return {"status": "UP"}
+
 
 @app.get("/version")
 
 
 def version():
     return {"version": "1.0.0"}
-
